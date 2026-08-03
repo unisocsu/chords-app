06b567880cb75a82b317f27fff19c2b075f0c79c
unisocsu <rmk0557603971@outlook.com>
2026-08-03 12:39:04 +0300

    Update NativeAudioEngine.java

--- app/src/main/java/com/chords/app/NativeAudioEngine.java
+++ app/src/main/java/com/chords/app/NativeAudioEngine.java
@@ -1,1 +1,9 @@
-// NativeAudioEngine.java  
+package com.chords.app;
+
+public class NativeAudioEngine {
+    static {
+        System.loadLibrary("chord_recognizer");
+    }
+
+    public native String processAudioBuffer(short[] buffer, int sampleRate);
+}
